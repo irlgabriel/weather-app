b893973c66393183d722a6cec022ed667b8ff2bd
irlgabriel <radu.gabriel131@gmail.com>
2020-10-13 08:56:27 +0300

    Updates

---
+++ precache-manifest.33d45fbab4c5bf1d7fe33a91bc539f44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e676487609deb42aa3457370fb2718e6",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "2d7ad41c7f769b01dec9",
+    "url": "/weather-app/static/js/2.c2767992.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.c2767992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0c37d70a03efb8ffea0",
+    "url": "/weather-app/static/js/main.91aa170c.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
