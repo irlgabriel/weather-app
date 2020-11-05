07f1dc4d3f41abcca9cf1267e858a2d4881d1c89
irlgabriel <radu.gabriel131@gmail.com>
2020-11-05 13:21:59 +0200

    styled it a bit

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -34,7 +34,7 @@ export default function Weather({
         weatherObj.list.map(
         day => 
             <Col className="m-0 justify-content-between" key={dayIndex} xs="12" sm="6" md="4">
-              <Card className="m-0 mt-3 bg-light text-dark" key={days[(dayIndex) % 7]}>
+              <Card className="m-0 mb-2 mt-3 bg-light text-dark" key={days[(dayIndex) % 7]}>
                 <CardHeader>
                   <CardTitle><h3>{days[(dayIndex ++) % 7]}</h3></CardTitle>
                   <CardSubtitle><em style={{fontSize: "1.3rem"}}>{day.weather[0].description}</em></CardSubtitle>
